644cb0662c82fdbb12266e535b4efee48e39d184
Huayong Kuang <sudohuoty@163.com>
2016-12-05 10:25:04 +0800

    用 exec 执行，以便于 supervisor 管理进程 站点目录使用全局路径，否则进程找不到

--- tools/runserver.sh
+++ tools/runserver.sh
@@ -7,4 +7,4 @@
 source /home/huoty/.virtualenvs/blog/bin/activate
 export PYTHONPATH="/home/huoty/luring/"
 export PYTHONUNBUFFERED=1
-python -m server -p 8800 -r _site/
+exec python -m server -p 8800 -r /home/huoty/luring/_site/
