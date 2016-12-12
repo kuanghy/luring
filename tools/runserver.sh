8afd990126ed648a1225b9fb3327d11e51e9749d
Huayong Kuang <sudohuoty@163.com>
2016-12-12 14:30:29 +0800

    recover runserver.sh

--- tools/runserver.sh
+++ tools/runserver.sh
@@ -4,17 +4,7 @@
 # Author: Huoty <sudohuoty@gmail.com>
 # Script starts from here:
 
-set -e
-
-if type activate >/dev/null 2>&1; then
-    source activate blog > /dev/null 2>&1
-elif type workon >/dev/null 2>&1; then
-    workon blog > /dev/null 2>&1
-else
-    echo "Error: Unable to switch working environment!"
-    exit 1
-fi
-
+source /home/huoty/.virtualenvs/blog/bin/activate
 export PYTHONPATH="/home/huoty/luring/"
 export PYTHONUNBUFFERED=1
 exec python -m server -p 8800 -r /home/huoty/luring/_site/
