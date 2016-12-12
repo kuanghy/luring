f4fbf5efb4ae446d80d1f42ae895dbc8635cbc91
Huayong Kuang <sudohuoty@163.com>
2016-12-12 12:56:38 +0800

    Post: Python代码调试工具 fix runserver add pushm, push master to all repo

--- tools/runserver.sh
+++ tools/runserver.sh
@@ -4,7 +4,17 @@
 # Author: Huoty <sudohuoty@gmail.com>
 # Script starts from here:
 
-source /home/huoty/.virtualenvs/blog/bin/activate
+set -e
+
+if type activate >/dev/null 2>&1; then
+    source activate blog > /dev/null 2>&1
+elif type workon >/dev/null 2>&1; then
+    source workon blog > /dev/null 2>&1
+else
+    echo "Error: Unable to switch working environment!"
+    exit 1
+fi
+
 export PYTHONPATH="/home/huoty/luring/"
 export PYTHONUNBUFFERED=1
 exec python -m server -p 8800 -r /home/huoty/luring/_site/
